8714a61f4e9e486a9839a9dd83210ce98d8424c5
Moreno Gomes <moreno.gomes@mail.utoronto.ca>
2020-03-27 14:10:10 -0400

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,30 @@
-const questions = [
-    prompt("What's your Github username?")
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const api = require("/utils/api");
+const generateMarkdown = require("utils/generateMarkdown");
+
+
+const questions = [ 
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+      },
+       {
+        type: "input",
+        name: "title",
+        message: "What is your project's name?"
+      } 
 ];
 
-function writeToFile(fileName, data) {
-}
 
-function init() {
+// function writeToFile(fileName, data) {
+//     return fs.
+// }
 
-}
+// function init() {
 
-init();
+// }
+
+// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,26 @@
+function generateProjectUrl(gitHub, title) { 
+  const kebabCaseTitle = title.toLowerCase().split(" ").join("-");
+  return `https://github.com/${gitHub}/${kebabCaseTitle}`;
+} 
+
+function renderLicense(license, gitHub, title) {
+  if(licence !== "None"){
+    return `[![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)](${generateProjectUrl(gitHub, title)})`
+  }
+  return " "
+}
+
+
+function renderLicenseResponse(license) {
+  if(licence !=="None"){
+    return (
+      `## License
+      This project is licensed under the ${license} license.`
+        )
+    }
+      return ''
+}
+
 function generateMarkdown(data) {
   return `
 # ${data.title}
